368bb415a7a0b7bfe6d4b286d6193c03c9daa4d5
Reagan Sasan <reagansasani@hotmail.com>
2021-08-17 16:32:37 +0200

    test

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <h1>test</h1>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
