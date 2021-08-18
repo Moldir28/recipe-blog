9fb29a5d6fe7e8c6b3e874122804ef767cab24da
Moldir28 <75260678+Moldir28@users.noreply.github.com>
2021-08-18 17:01:01 +0400

    committing changes on the SM part

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,26 +1,24 @@
 import React, {useState} from 'react'
 import './Footer.css'
+import {FiFacebook} from 'react-icons/fi'; 
+import {FiInstagram} from 'react-icons/fi'
+
+
+
 
 function Footer() {
-    const [input, setInput] = useState('email')
-
-    const handleChange = ({target}) => {
-      setInput(target.value)
-    }
-
-
+ 
     return (
         <div className='footer'>
             <h4 className='webName'>Food Loose</h4>
             <div className='subscribe'>
               <h5> SUBSCRIBE FOR MORE RECIPES</h5>
-              <input type='text' onChange={handleChange} value={input} className='input'/>
+              <input type='text'  placeholder='email' className='input'/>
             </div>
             <div className='logos'>
-                <img src='././Media/facebook.png' alt='facebook' />
-                <img src='././Media/insta.png' alt='instagram' />
-                <img src='././Media/pinterest.png' alt='pinterest'/>
-                <img src='././Media/twitter.png' alt='twitter'/>
+                <FiFacebook className='smLogo' />
+                <FiInstagram className='smLogo' />
+        
             </div>
         </div>
     )
