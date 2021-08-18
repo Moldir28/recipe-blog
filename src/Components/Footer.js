4ba564b9a8ea9c3c25556f788007dc77d569e114
Moldir28 <75260678+Moldir28@users.noreply.github.com>
2021-08-18 14:05:08 +0400

    commit footer content

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Footer() {
+    const [input, setInput] = useState('type your email')
+
+    const handleChange = ({target}) => {
+      setInput(target.value)
+    }
+
+
     return (
-        <div>
-            
+        <div className='footer'>
+            <h4>Food Loose</h4>
+            <div className='subscribe'>
+            <h5> Subscribe for more recipes </h5>
+            <input type='text' onChange={handleChange} value={input}/>
+            </div>
+            <div>
+                <img />
+                <img />
+                <img />
+                <img />
+            </div>
         </div>
     )
 }
